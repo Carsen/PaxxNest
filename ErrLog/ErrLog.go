73980020c4d335e5d0e0b77e137fd9d19d26e773
Carsen <Carsenebert@gmail.com>
2024-08-06 17:46:14 -0500

    Use pointers to a log file

--- ErrLog/ErrLog.go
+++ ErrLog/ErrLog.go
@@ -7,10 +7,17 @@ import (
 
 func LogErr(a error) {
 	LogFile, err := os.OpenFile("errors.log", os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0666)
-	defer LogFile.Close()
 	if err != nil {
 		log.Fatalf("Failed to open log file: %v", err)
 	}
-	log.SetOutput(LogFile)
-	log.Print(a)
+	defer LogFile.Close()
+	L := log.New(LogFile, "Error: ", log.Ldate|log.Ltime|log.Lshortfile)
+	Logger(a, L)
+
+}
+
+func Logger(err error, logger *log.Logger) {
+	if err != nil {
+		logger.Print(err)
+	}
 }
--- Manager/go.mod
+++ Manager/go.mod
@@ -2,4 +2,4 @@ module github.com/Carsen/PaxxNest/Manager
 
 go 1.22.5
 
-require github.com/Carsen/PaxxNest v0.0.0-20240806222234-632c1738d25a // indirect
+require github.com/Carsen/PaxxNest v0.0.0-20240806222919-11787d5c859c // indirect
