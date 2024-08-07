949372c2e6b68eea9a0dcd6edb3cd446cd406980
Carsen <Carsenebert@gmail.com>
2024-08-07 16:13:50 -0500

    QOL Updates

--- src/main.go
+++ src/main.go
@@ -2,21 +2,25 @@ package main
 
 import (
 	"fmt"
-
+	
 	"github.com/Carsen/PaxxNest/Manager"
 )
 
 func main() {
-		mgr := Manager.NewManager()
-		mgr.AddManager("brew", Manager.BrewMan{})        //	Uncomment this line for Homebrew integration
-		mgr.AddManager("snap", Manager.SnapMan{})        //	Uncomment this line for Snap integration
-		mgr.AddManager("pacman", Manager.PacMan{})       //	Uncomment this line for Pacman integration
-		mgr.AddManager("winget", Manager.WingetMan{})    //	Uncomment this line for Winget integration
-		mgr.AddManager("scoop", Manager.ScoopMan{})      //	Uncomment this line for Scoop integration
-		mgr.AddManager("chocolatey", Manager.ChocoMan{}) //	Uncomment this line for Chocolatey integration
-		mgr.AddManager("npm", Manager.NpmMan{})          //	Uncomment this line for NPM integration
-		mgr.AddManager("nix", Manager.NixMan{})			//	Uncomment this line for Nix Integration
+	mgr := Manager.NewManager()
+	mgr.AddManager("brew", Manager.BrewMan{})        //	Uncomment this line for Homebrew integration
+	mgr.AddManager("snap", Manager.SnapMan{})        //	Uncomment this line for Snap integration
+	mgr.AddManager("pacman", Manager.PacMan{})       //	Uncomment this line for Pacman integration
+	mgr.AddManager("winget", Manager.WingetMan{})    //	Uncomment this line for Winget integration
+	mgr.AddManager("scoop", Manager.ScoopMan{})      //	Uncomment this line for Scoop integration
+	mgr.AddManager("chocolatey", Manager.ChocoMan{}) //	Uncomment this line for Chocolatey integration
+	mgr.AddManager("npm", Manager.NpmMan{})          //	Uncomment this line for NPM integration
+	mgr.AddManager("nix", Manager.NixMan{})			//	Uncomment this line for Nix Integration
 
+	var usern string
+	fmt.Print("Please enter username:")
+	fmt.Scanln(&usern)
+	for len(usern) > 0 {
 		fmt.Println("Press 'L' for list, 'I' for install, and 'R' for remove")
 		var choice string
 		fmt.Print("> ")
@@ -37,4 +41,5 @@ func main() {
 			fmt.Scanln(&whichpkg)
 			mgr.RemovePackage(whichpkg)
 		}
+	}
 }
